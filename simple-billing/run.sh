78b6df5c974a3ff04cb09b528dc9f091337a342e
Soumen Karmakar <soumen.karmakar@outlook.com>
2023-06-13 15:39:43 +0100

    Update run.sh

--- simple-billing/run.sh
+++ simple-billing/run.sh
@@ -1,5 +1,5 @@
 docker run -d \
-  -e MONGO_URI=mongodb+srv://billingapp:R177U3SVwh65EW0M@cluster0.jyf70cj.mongodb.net/billingapp \
+  -e MONGO_URI=mongodb+srv://<username>:<password>@<clusterAddress>/billingapp \
   -e MONGO_DB_NAME=billingapp \
   -p "9191:8080" \
   itssoumen/simple-billing
